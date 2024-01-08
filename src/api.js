1ad9a6974c9627ca0b2bd8a680f17c8c9393eaa5
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-01-08 12:15:51 -0500

    Updated auth url

--- src/api.js
+++ src/api.js
@@ -79,7 +79,7 @@ export const getAccessToken = async () => {
     const code = await searchParams.get("code");
     if (!code) {
       const response = await fetch(
-        "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+        "https://aljbzhip67.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
       );
       const result = await response.json();
       const { authUrl } = result;
