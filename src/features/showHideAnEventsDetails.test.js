d8618521db6abf1695025668d4e84ca2accc95ef
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-01-09 13:48:36 -0500

    Finished first scenario acceptance test

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,24 +1,42 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, waitFor, within } from '@testing-library/react';
 import { getEvents } from '../api';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
+import Event from '../components/Event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
 
   test('Events are collapsed by default', ({ given, when, then }) => {
-    given('the Meet app is open', () => {
 
+    let AppComponent;
+    let AppDOM;
+    let EventListDOM;
+    let eventComponent
+    let mockEvent;
+
+    beforeAll(async () => {
+      const allEvents = await getEvents();
+      mockEvent = allEvents[0];
+    });
+
+    beforeEach(() => {
+      eventComponent = render(<Event event={mockEvent} />);
+    });
+
+    given('the Meet app is open', () => {
+      AppComponent = render(<App />);
     });
 
     when('the user views the list of events', () => {
-
+      const AppDom = AppComponent.container.firstChild;
+      const EventListDOM = AppDom.querySelector('#event-list');
+      expect(EventListDOM).toBeInTheDocument;
     });
 
     then('each event should be collapsed by default.', () => {
-
+      expect(eventComponent.queryByText(mockEvent.description)).not.toBeInTheDocument();
     });
 });
 
